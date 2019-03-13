edf986c299b310905e41f746adfbe5ebc1461c31
eisenfox <nat.eisen.davydova@gmail.com>
2019-03-13 17:02:32 +0300

    JS work: changing demo classes

--- js/index.js
+++ js/index.js
@@ -2,9 +2,11 @@
 
 //DEMO CODE - !!!YOU DON'T NEED IT
 var demoBtns = document.querySelectorAll('.demo-btn');
+var nav = document.querySelector('.nav');
+var pageContent = document.querySelector('.page__content');
 
 //change demo on btn click
-demoBtns.forEach(function(el) {
+demoBtns.forEach(function(el, ind) {
   
   el.addEventListener('click', function() {
     
@@ -14,6 +16,19 @@ demoBtns.forEach(function(el) {
     });
     
     this.classList.add('js-active');
+    
+    //add proper classes for effects
+    var effClass = ind + 1;
+    
+    nav.classList.remove(nav.classList[1]);
+    
+    nav.classList.add('nav--offcanvas-' + effClass);
+    
+    pageContent.classList.remove(pageContent.classList[1]);
+    
+ pageContent.classList.add('content--offcanvas-' + effClass);
+    
+    
     
   });
   
